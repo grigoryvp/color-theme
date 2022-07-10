a3838c703cbe149f2f5b5b9ad08a442a5a7e94fd
Grigory Petrov <grigoryvp@gmail.com>
2022-07-10 19:09:10 +0300

    Parameter names in js and tsx

---
+++ test/test.tsx
@@ -0,0 +1,14 @@
+// Line comment
+/* Block comment */
+class Foo extends Object {
+  _foo = [1, .1, 0xa, Infinity, Math.PI];
+  foo(bar, baz) {
+    for (const foo of [null, undefined, true, NaN]) {
+      Array.prototype.push();
+      this._foo = {
+        foo: ["stri\ng", 'stri\ng', `stri\ng ${foo}`, /reg\\exp$/]
+      };
+      return 10;
+    }
+  }
+}
