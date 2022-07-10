96789c5dece157402932e81e9060f735c255ae58
Grigory Petrov <grigoryvp@gmail.com>
2022-07-10 22:17:23 +0300

    Examples with less warnings

--- test/test.js
+++ test/test.js
@@ -1,15 +1,13 @@
 // Line comment
 /* Block comment */
-class Foo extends Object {
-  _foo = [1, .1, 0xa, Infinity, Math.PI];
+class Foo1 extends Object {
+  _foo = [1, .1, 0xa, Infinity, Math.PI, "", /./];
   foo(bar, baz) {
-    bar = baz = 1
     for (const foo of [null, undefined, true, NaN]) {
       Array.prototype.push();
-      this._foo = {
-        foo: ["stri\ng", 'stri\ng', `stri\ng ${foo}`, /reg\\exp$/]
-      };
-      return 10;
+      this._foo = ["stri\ng", 'stri\ng', `stri\ng ${foo}`, /reg\\exp$/];
+      return {foo: this._foo};
     }
   }
 }
+console.log("foo");
--- test/test.tsx
+++ test/test.tsx
@@ -1,14 +1,12 @@
 // Line comment
 /* Block comment */
-class Foo extends Object {
-  _foo: any = [1, .1, 0xa, Infinity, Math.PI];
+class Foo2 extends Object {
+  _foo = [1, .1, 0xa, Infinity, Math.PI, "", /./];
   foo(bar: any, baz: any) {
     for (const foo of [null, undefined, true, NaN]) {
       Array.prototype.push();
-      this._foo = {
-        foo: ["stri\ng", 'stri\ng', `stri\ng ${foo}`, /reg\\exp$/]
-      };
-      return 10;
+      this._foo = ["stri\ng", 'stri\ng', `stri\ng ${foo}`, /reg\\exp$/];
+      return {foo: this._foo};
     }
   }
 }
